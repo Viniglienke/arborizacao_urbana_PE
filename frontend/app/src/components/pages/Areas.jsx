c411e31514a05b301dc5471c1deee8e64b1e0477
Wilian Zembrani <Wilian3355@gmail.com>
2024-07-13 12:38:17 -0300

    Tabela Áreas atualizada

--- frontend/app/src/components/pages/Areas.jsx
+++ frontend/app/src/components/pages/Areas.jsx
@@ -1,5 +1,126 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Axios from "axios";
+import './Areas.css';
 
+const Areas = () => {
+    const [values, setValues] = useState({
+        areaname: "",
+        arealocalization: "",
+        areasize: "",
+        creationdate: "",
+        usercpf: ""
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setValues((prevValues) => ({
+            ...prevValues,
+            [e.target.name]: e.target.value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const formattedDate = formatDate(values.plantingDate);
+
+        Axios.post("http://localhost:3001/Areas", {
+            areaname: values.areaname,
+            creationdate: formattedDate,
+            arealocalization: values.arealocalization,
+            areasize: values.areasize,
+            usercpf: values.usercpf
+        })
+        .then((response) => {
+            console.log(response.data); 
+            alert("Area regisrtada com sucesso! Redirecionando...");
+            navigate("/");
+        })
+        .catch((error) => {
+            console.error("Erro ao registrar Area:", error);
+            alert("Erro ao registrar Area. Verifique o console para mais detalhes.");
+        });
+    };
+
+    const formatDate = (dateString) => {
+        const dateObj = new Date(dateString);
+        const day = dateObj.getDate();
+        const month = dateObj.getMonth() + 1;
+        const year = dateObj.getFullYear();
+        return `${day}/${month}/${year}`;
+    };
+
+    return (
+        <div className="container">
+            <form onSubmit={handleSubmit}>
+                <h1>Registrar Area</h1>
+                <div className="input-field">
+                    <input
+                        type="text"
+                        placeholder="Nome da Area"
+                        required
+                        id="areaname"
+                        name="areaname"
+                        value={values.areaname}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="input-field">
+                    <input
+                        type="date"
+                        placeholder="Data de criacao da area"
+                        required
+                        id="creationdate"
+                        name="creationdate"
+                        value={values.creationdate}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="input-field">
+                    <input
+                        type="text"
+                        placeholder="Tamnho da Area"
+                        required
+                        id="areasize"
+                        name="areasize"
+                        value={values.areasize}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="input-field">
+                    <input
+                        type="text"
+                        placeholder="Localização"
+                        required
+                        id="arealocalization"
+                        name="arealocalization"
+                        value={values.arealocalization}
+                        onChange={handleChange}
+                    />
+                </div>
+                <button type="submit">Registrar Area</button>
+            </form>
+        </div>
+    );
+};
+
+export default Areas;
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 function Areas() {
   return (
     <div>
@@ -9,4 +130,4 @@ function Areas() {
   );
 }
 
-export default Areas;
+export default Areas;*/
--- frontend/app/src/components/pages/Monitoring.jsx
+++ frontend/app/src/components/pages/Monitoring.jsx
@@ -3,49 +3,49 @@ import axios from 'axios';
 import './Monitoring.css';
 
 const Monitoring = () => {
-  const [trees, setTrees] = useState([]);
+const [trees, setTrees] = useState([]);
 
-  useEffect(() => {
-      fetchTrees();
-  }, []);
+useEffect(() => {
+    fetchTrees();
+}, []);
 
-  const fetchTrees = async () => {
-      try {
-          const response = await axios.get('http://localhost:3001/trees');
-          console.log(response.data); // Verifique os dados recebidos do backend
-          setTrees(response.data);
-      } catch (error) {
-          console.error("Erro ao buscar árvores:", error);
-      }
-  };
+const fetchTrees = async () => {
+    try {
+        const response = await axios.get('http://localhost:3001/trees');
+        console.log(response.data); // Verifique os dados recebidos do backend
+        setTrees(response.data);
+    } catch (error) {
+        console.error("Erro ao buscar árvores:", error);
+    }
+};
 
-  return (
-      <div className="monitoring-container">
-          <h1 className='monitoring-title'>Monitoramento de Árvores</h1>
-          <table>
-              <thead>
-                  <tr>
-                      <th>ID</th>
-                      <th>Nome Científico</th>
-                      <th>Data de Plantio</th>
-                      <th>Estado de Saúde</th>
-                      <th>Localização</th>
-                  </tr>
-              </thead>
-              <tbody>
-                  {trees.map(tree => (
-                      <tr key={tree.id}>
-                          <td>{tree.id}</td>
-                          <td>{tree.tipo_arvore_id}</td>
-                          <td>{tree.data_plantio}</td>
-                          <td>{tree.estado_saude}</td>
-                          <td>{tree.area_id}</td>
-                      </tr>
-                  ))}
-              </tbody>
-          </table>
-      </div>
-  );
+return (
+    <div className="monitoring-container">
+        <h1 className='monitoring-title'>Monitoramento de Árvores</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome Científico</th>
+                    <th>Estado de Saúde</th>
+                    <th>Localização</th>
+                    <th>Data de Plantio</th>
+                </tr>
+            </thead>
+            <tbody>
+                {trees.map(tree => (
+                    <tr key={tree.id}>
+                        <td>{tree.id}</td>
+                        <td>{tree.tipo_arvore_id}</td>
+                        <td>{tree.data_plantio}</td>
+                        <td>{tree.estado_saude}</td>
+                        <td>{tree.area_id}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
+);
 };
 
 export default Monitoring;
