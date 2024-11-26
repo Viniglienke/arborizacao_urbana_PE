4f65d535dae8a068f57a97eec4df12b55e49ec0a
Wilian Zembrani <Wilian3355@gmail.com>
2024-11-25 22:17:30 -0300

    Pequenas atualizações

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const saltRounds = 10;
 const db = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "vini123",
+    password: "Julia,wilian2409",
     database: "biourb",
 });
 
