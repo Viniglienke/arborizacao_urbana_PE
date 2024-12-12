cf6b7951a9063c50e1bc3bddc479e4f8311d3b41
Vinícius Glienke <viniglienke@gmail.com>
2024-12-11 22:28:00 -0300

    api changes

--- frontend/app/src/services/api.js
+++ frontend/app/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://arborizacao-urbana-inezl03v5-viniglienkes-projects.vercel.app",
+  baseURL: "https://biourb.vercel.app/",
 });
