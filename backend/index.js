61235f350aa1c5df686fddde0aae767b69e7577a
Vinícius Glienke <viniglienke@gmail.com>
2024-12-11 22:20:10 -0300

    ,

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const db = new Pool({
 app.use(express.json());
 const corsOptions = {
     credentials: true,
-    origin: 'https://biourb.vercel.app',
+    origin: ['https://biourb.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 };
